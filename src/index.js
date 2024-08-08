1bf7c9acd587d11e7405bcf85b272eb4cabcbb52
Arnav <arnav.kawale@gmail.com>
2024-08-08 13:12:59 +0530

    commit all

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+dotenv.config({
+    path:'./.env'
+
+})
+connectDB().then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`server is running at port: ${process.env.PORT}`);
+
+        
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB db connection failed!!!",err);
+    
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+ 
+export {asyncHandler}
+
+
+// const asyncHandler =(fn)=>async(req,res,next)=>{
+//     try {
+        
+//     } catch (error) {
+//         res.send(err.code||500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// } 
